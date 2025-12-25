4fdccb8fa157d7394e97e8ce57d779855f8a3aaa
pharisnyannyan <137247845+pharisnyannyan@users.noreply.github.com>
2025-12-25 17:36:46 +0900

    Add syntax checker stage

---
+++ 03_checker/examples/main_checker.c
@@ -0,0 +1,19 @@
+#include "checker.h"
+
+#include <stdio.h>
+
+int main(void)
+{
+    const char *source = "int value = 7;";
+    Checker checker;
+
+    checker_init(&checker, source);
+
+    if (!checker_check(&checker)) {
+        fprintf(stderr, "checker error: %s\n", checker_error(&checker));
+        return 1;
+    }
+
+    printf("syntax ok\n");
+    return 0;
+}

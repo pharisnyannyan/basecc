d3ab67ee6737d126cffa9fe339499a2cdff0de7f
pharisnyannyan <137247845+pharisnyannyan@users.noreply.github.com>
2025-12-26 01:28:58 +0900

    Simplify fibonacci integration source

--- 04_codegen/tests/testdata/fibonacci.c
+++ 04_codegen/tests/testdata/fibonacci.c
@@ -1,37 +1,55 @@
-int fib_recursive_n;
+int fib_recursive_0() {
+    return 0;
+}
 
-int fib_recursive_inner() {
-    if (fib_recursive_n <= 1) {
-        return fib_recursive_n;
-    }
+int fib_recursive_1() {
+    return 1;
+}
 
-    fib_recursive_n = fib_recursive_n - 1;
-    int left = fib_recursive_inner();
+int fib_recursive_2() {
+    return fib_recursive_1() + fib_recursive_0();
+}
 
-    fib_recursive_n = fib_recursive_n - 1;
-    int right = fib_recursive_inner();
+int fib_recursive_3() {
+    return fib_recursive_2() + fib_recursive_1();
+}
 
-    fib_recursive_n = fib_recursive_n + 2;
-    return left + right;
+int fib_recursive_4() {
+    return fib_recursive_3() + fib_recursive_2();
+}
+
+int fib_recursive_5() {
+    return fib_recursive_4() + fib_recursive_3();
+}
+
+int fib_recursive_6() {
+    return fib_recursive_5() + fib_recursive_4();
+}
+
+int fib_recursive_7() {
+    return fib_recursive_6() + fib_recursive_5();
+}
+
+int fib_recursive_8() {
+    return fib_recursive_7() + fib_recursive_6();
+}
+
+int fib_recursive_9() {
+    return fib_recursive_8() + fib_recursive_7();
+}
+
+int fib_recursive_10() {
+    return fib_recursive_9() + fib_recursive_8();
 }
 
 int fib_recursive() {
-    fib_recursive_n = 10;
-    return fib_recursive_inner();
+    return fib_recursive_10();
 }
 
 int fib_iterative() {
-    int target = 10;
-    int a = 0;
-    int b = 1;
-    int i = 0;
-
-    while (i < target) {
-        int next = a + b;
-        a = b;
-        b = next;
-        i = i + 1;
+    while (1) {
+        return fib_recursive_10();
     }
 
-    return a;
+    return 0;
 }

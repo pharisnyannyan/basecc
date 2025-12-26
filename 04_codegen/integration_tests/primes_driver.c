50634897ad3d3d0b36d939a85745ed643d3b3c53
pharisnyannyan <137247845+pharisnyannyan@users.noreply.github.com>
2025-12-26 13:09:56 +0900

    Add primes integration test

---
+++ 04_codegen/integration_tests/primes_driver.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int fill_primes(int *buffer, int max);
+
+int main(void)
+{
+    int buffer[100];
+    int count = fill_primes(buffer, 100);
+
+    for (int i = 0; i < count; i++) {
+        if (i) {
+            printf(" ");
+        }
+        printf("%d", buffer[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
